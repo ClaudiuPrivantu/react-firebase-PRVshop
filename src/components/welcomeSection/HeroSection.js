b662447ed103039e603245ae3bf74e70525ab4ca
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2022-12-04 16:58:59 +0200

    added video src

--- src/components/welcomeSection/HeroSection.js
+++ src/components/welcomeSection/HeroSection.js
@@ -7,6 +7,7 @@ function HeroSection() {
     <div className='hero-container'>
       <h1>PRV SHOP</h1>
       <p>if you can't stop thinking about it... BUY IT</p>
+      <video src='videos/video.mp4' autoPlay loop muted></video>
     </div>
   );
 }
